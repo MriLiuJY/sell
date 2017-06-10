05a6067197d55ecd726bfafc2500f9f2de72145e
MriLiuJY <1171639020@qq.com>
2017-06-10 11:09:22 +0800

    06-10

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -7,6 +7,7 @@
         <div class="back" @click="hide">
           <i class="icon-arrow_lift"></i>
         </div>
+      </div>
       <div class="contented">
         <h1 class="titleed">{{food.name}}</h1>
         <div class="detail">
@@ -21,10 +22,18 @@
         </div>
         <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
       </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="titletop">商品信息</h1>
+          <p class="texted">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="titlenext">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
-      <split></split>
-  </div>
   </transition>
 </template>
 
@@ -32,7 +41,12 @@
   import BScroll from 'better-scroll'
   import Vue from 'vue'
   import cartcontrol from 'component/cartcontrol/cartcontrol.vue'
+  import ratingselect from 'component/ratingselect/ratingselect.vue'
   import split from 'component/split/split.vue'
+
+  const POSITIVE = 0
+  const NEGATIVE = 1
+  const ALL = 2
 
   export default {
     props: {
@@ -42,18 +56,27 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       }
     },
     methods: {
       show() {
         this.showFlag = true
+        this.selectType = ALL
+        this.onlyContent = true
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
               click: true
             })
-          }else{
+          } else {
             this.scroll.refresh()
           }
         })
@@ -70,6 +93,7 @@
     },
     components: {
       cartcontrol,
+      ratingselect,
       split
     }
   }
@@ -167,4 +191,28 @@
           color: #fff
           background: rgb(0,160,220)
         }
+    .info{
+      padding: 18px
+    }
+    .titletop{
+      line-height: 14px
+      margin-right: 6px
+      font-size: 14px
+      color: rgb(7,17,27)
+    }
+    .texted{
+      line-height:24px
+      font-size: 12px
+      padding: 0 8px
+      color: rgb(77,85,93)
+    }
+    .rating{
+      padding-top: 18px
+    }
+    .titlenext{
+      line-height: 14px
+      font-size: 14px
+      color: rgb(7,17,27)
+      margin-left: 18px
+    }
 </style>
